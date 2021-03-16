6e540bc3467ec7eec664e7b452da6a6fdb8929f1
ablue968 <ablue968@gmail.com>
2021-03-16 20:09:28 +0000

    need to add clear all button

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,108 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+var link = "https://assets.breatheco.de/apis/fake/todos/user/ablue968";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			contacts: [],
+			input: ""
+		};
+	}
+
+	addListItems = e => {
+		let key = e.which || e.keyCode || 0;
+		if (key !== 13) return;
+
+		let newContacts = this.state.contacts;
+		let newObject = { label: this.state.input, done: false };
+		newContacts.push(newObject);
+		fetch(link, {
+			method: "PUT",
+			body: JSON.stringify(newContacts),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(newRes => newRes.text())
+			.then(response => {
+				console.log(response);
+				this.setState({
+					contacts: newContacts,
+					input: ""
+				});
+			})
+			.catch(error => console.error("Error:", error));
+	};
+
+	deleteListItems = index => {
+		let updatedContacts = this.state.contacts;
+		updatedContacts[index].done = true;
+		fetch(link, {
+			method: "PUT",
+			body: JSON.stringify(updatedContacts),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(newRes => newRes.text())
+			.then(response => {
+				console.log(response);
+				this.setState({
+					contacts: updatedContacts
+				});
+			})
+			.catch(error => console.error("Error:", error));
+	};
+
+	render() {
+		return (
+			<div className="container-fluid">
+				<div className="header">
+					<h1>Todos</h1>
+				</div>
+				<div className="text-center toDos">
+					<input
+						id="addItem"
+						type="text"
+						className="mb-3"
+						placeholder="What needs to be done?"
+						onKeyPress={this.addListItems}
+						value={this.state.input}
+						onChange={e => this.setState({ input: e.target.value })}
+					/>
+					<ul className="mx-auto">
+						{this.state.contacts.map((item, index) => {
+							return (
+								<li
+									className="flexItems d-flex justify-content-between"
+									key={index}>
+									<div className="listItem">
+										{item.label} {item.done && " - Done"}
+									</div>
+									<div className="deleter">
+										<button
+											type="button"
+											className="btn btn-outline-danger"
+											onClick={() =>
+												this.deleteListItems(index)
+											}>
+											X
+										</button>
+									</div>
+								</li>
+							);
+						})}
+						<li className=" d-flex justify-content-center">
+							{this.state.contacts.length === 1
+								? this.state.contacts.length + " total item"
+								: this.state.contacts.length + " total items"}
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
